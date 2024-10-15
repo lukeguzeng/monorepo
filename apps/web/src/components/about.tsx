52573874dafff34b5e557f54c49fe6110edc0c3b
Gu Zeng <guzeng0516@gmail.com>
2024-10-15 00:10:36 -0700

    better routing

---
+++ apps/web/src/components/about.tsx
@@ -0,0 +1,5 @@
+function About() {
+  return <h1 className="text-8xl font-bold underline text-cyan-500">About!</h1>;
+}
+
+export default About;
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,61 +1,13 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  Outlet,
   RouterProvider,
-  Link,
-  createRouter,
-  createRoute,
-  createRootRoute,
 } from '@tanstack/react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 import './style.css';
+import { router } from './routes/__root';
 
-const rootRoute = createRootRoute({
-  component: () => (
-    <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{' '}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-      </div>
-      <hr />
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
-  ),
-});
-
-const indexRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: '/',
-  component: function Index() {
-    return (
-      <div className="p-2">
-        <h3>Welcome Home!</h3>
-      </div>
-    );
-  },
-});
-
-const aboutRoute = createRoute({
-  getParentRoute: () => rootRoute,
-  path: '/about',
-  component: function About() {
-    return (
-      <h1 className="text-8xl font-bold underline text-cyan-500">About!</h1>
-    );
-  },
-});
-
-const routeTree = rootRoute.addChildren([indexRoute, aboutRoute]);
-
-const router = createRouter({ routeTree });
 
 declare module '@tanstack/react-router' {
   interface Register {
