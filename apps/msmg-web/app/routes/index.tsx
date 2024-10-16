36e9149036e781c76576773cde4bebaaf439e9ff
Gu Zeng <guzeng0516@gmail.com>
2024-10-16 14:25:29 -0700

    add tailwind css

--- apps/msmg-web/app/routes/index.tsx
+++ apps/msmg-web/app/routes/index.tsx
@@ -1,43 +1,67 @@
-// app/routes/index.tsx
-import * as fs from 'fs'
-import { createFileRoute, useRouter } from '@tanstack/react-router'
-import { createServerFn } from '@tanstack/start'
+import "./globals.css";
+import "@repo/ui/styles.css";
+import * as fs from 'fs';
+import { createFileRoute, useRouter } from '@tanstack/react-router';
+import { createServerFn } from '@tanstack/start';
+import { Card } from '@repo/ui/card';
 
-const filePath = 'count.txt'
+const filePath = 'count.txt';
+
+const LINKS = [
+  {
+    title: "Docs",
+    href: "https://turbo.build/repo/docs",
+    description: "Find in-depth information about Turborepo features and API.",
+  },
+  {
+    title: "Learn",
+    href: "https://turbo.build/repo/docs/handbook",
+    description: "Learn more about monorepos with our handbook.",
+  },
+];
 
 async function readCount() {
   return parseInt(
-    await fs.promises.readFile(filePath, 'utf-8').catch(() => '0'),
-  )
+    await fs.promises.readFile(filePath, 'utf-8').catch(() => '0')
+  );
 }
 
 const getCount = createServerFn('GET', () => {
-  return readCount()
-})
+  return readCount();
+});
 
 const updateCount = createServerFn('POST', async (addBy: number) => {
-  const count = await readCount()
-  await fs.promises.writeFile(filePath, `${count + addBy}`)
-})
+  const count = await readCount();
+  await fs.promises.writeFile(filePath, `${count + addBy}`);
+});
 
 export const Route = createFileRoute('/')({
   component: Home,
   loader: async () => await getCount(),
-})
+});
 
 function Home() {
-  const router = useRouter()
-  const state = Route.useLoaderData()
+  const router = useRouter();
+  const state = Route.useLoaderData();
 
   return (
-    <button
-      onClick={() => {
-        updateCount(1).then(() => {
-          router.invalidate()
-        })
-      }}
-    >
-      Add 1 to {state}?
-    </button>
-  )
+    <>
+      <button
+        onClick={() => {
+          updateCount(1).then(() => {
+            router.invalidate();
+          });
+        }}
+      >
+        Add 1 to {state}?
+      </button>
+      <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        {LINKS.map(({ title, href, description }) => (
+          <Card href={href} key={title} title={title}>
+            {description}
+          </Card>
+        ))}
+      </div>
+    </>
+  );
 }
