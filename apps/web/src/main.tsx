2aa1d49d27b8276ba7ce1d3cf3ecc5fa3c74c9c6
Gu Zeng <guzeng0516@gmail.com>
2024-10-16 11:36:33 -0700

    switch to file route

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,29 +1,28 @@
-import { StrictMode } from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  RouterProvider,
-} from '@tanstack/react-router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+
+// Import the generated route tree
+import { routeTree } from './routeTree.gen'
 import './style.css';
-import { router } from './routes/__root';
 
+// Create a new router instance
+const router = createRouter({ routeTree })
 
+// Register the router instance for type safety
 declare module '@tanstack/react-router' {
   interface Register {
-    router: typeof router;
+    router: typeof router
   }
 }
-const queryClient = new QueryClient();
-const rootElement = document.getElementById('app')!;
+
+// Render the app
+const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
-  const root = ReactDOM.createRoot(rootElement);
+  const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </StrictMode>
-  );
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
 }
--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,49 +1,20 @@
-import {
-  Outlet,
-  Link,
-  createRouter,
-  createRoute,
-  createRootRoute,
-} from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
-import About from '../components/about';
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 
-const rootRoute = createRootRoute({
-    component: () => (
-      <>
-        <div className="p-2 flex gap-2">
-          <Link to="/" className="[&.active]:font-bold">
-            Home
-          </Link>{' '}
-          <Link to="/about" className="[&.active]:font-bold">
-            About
-          </Link>
-        </div>
-        <hr />
-        <Outlet />
-        <TanStackRouterDevtools />
-      </>
-    ),
-  });
-  
-  const indexRoute = createRoute({
-    getParentRoute: () => rootRoute,
-    path: '/',
-    component: function Index() {
-      return (
-        <div className="p-2">
-          <h3>Welcome Home!</h3>
-        </div>
-      );
-    },
-  });
-  
-  const aboutRoute = createRoute({
-    getParentRoute: () => rootRoute,
-    path: '/about',
-    component: About,
-  });
-  
-  const routeTree = rootRoute.addChildren([indexRoute, aboutRoute]);
-  
-  export const router = createRouter({ routeTree });
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <div className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{' '}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+})
