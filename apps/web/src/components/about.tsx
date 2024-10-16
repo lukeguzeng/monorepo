e25f5b9f709dca1b95bebd0a943a1e24e9a94b24
Turbobot <turbobot@vercel.com>
2024-10-16 13:16:02 -0700

    feat(create-turbo): apply official-starter transform

--- apps/web/src/components/about.tsx
+++
@@ -1,5 +0,0 @@
-function About() {
-  return <h1 className="text-8xl font-bold underline text-cyan-500">About!</h1>;
-}
-
-export default About;
--- apps/web/src/contexts/router.context.ts
+++
@@ -1,5 +0,0 @@
-import { QueryClient } from '@tanstack/react-query';
-
-export interface RouterContext {
-  queryClient: QueryClient;
-}
