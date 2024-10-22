f8cc7fbe6c8c237657874dc73f612b570875cdc6
Gu Zeng <guzeng0516@gmail.com>
2024-10-21 23:37:13 -0700

    use ui comp in wimg-web

--- apps/wimg-web/app/routes/index.tsx
+++ apps/wimg-web/app/routes/index.tsx
@@ -1,13 +1,35 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+import { Card } from '@repo/ui/card';
 
 export const Route = createFileRoute('/')({
   component: Home,
-})
+});
+
+const LINKS = [
+  {
+    title: 'Docs',
+    href: 'https://turbo.build/repo/docs',
+    description: 'Find in-depth information about Turborepo features and API.',
+  },
+  {
+    title: 'Learn',
+    href: 'https://turbo.build/repo/docs/handbook',
+    description: 'Learn more about monorepos with our handbook.',
+  },
+];
 
 function Home() {
   return (
     <div className="p-2">
       <h3>Welcome Home!!!</h3>
+      <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        {LINKS.map(({ title, href, description }) => (
+          <Card href={href} key={title} title={title}>
+            {description}
+          </Card>
+        ))}
+      </div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
-  )
+  );
 }
